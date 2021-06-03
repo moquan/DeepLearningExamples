0c468a2491383a30903ba8287b01898731b53a1d
moquan <moquan@users.noreply.github.com>
2021-06-03 15:34:58 +0100

    Update train_tacotron2_speaker_2_stage.sh

--- PyTorch/SpeechSynthesis/CUED_Tacotron2/CUED_scripts/train_tacotron2_speaker_2_stage.sh
+++ PyTorch/SpeechSynthesis/CUED_Tacotron2/CUED_scripts/train_tacotron2_speaker_2_stage.sh
@@ -1,4 +1,4 @@
 mkdir -p output_tacotron2_speaker_2_stage
-python -m multiproc train.py -m Tacotron2_Speaker_2_Stage -o ./output_tacotron2/ -lr 1e-3 --epochs 1501 -bs 16 --weight-decay 1e-6 --grad-clip-thresh 1.0 --cudnn-enabled --anneal-steps 500 1000 1500 --anneal-factor 0.1 \
+python -m multiproc train.py -m Tacotron2_Speaker_2_Stage -o ./output_tacotron2_speaker_2_stage/ -lr 1e-3 --epochs 1501 -bs 16 --weight-decay 1e-6 --grad-clip-thresh 1.0 --cudnn-enabled --anneal-steps 500 1000 1500 --anneal-factor 0.1 \
   --log-file train_tacotron2.json  --load-mel-from-disk True \
   --training-files VCTK-Corpus/filelists/2_stage_exp/mel_text_train_filelist.txt --validation-files VCTK-Corpus/filelists/2_stage_exp/mel_text_valid_filelist.txt
